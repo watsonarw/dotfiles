14c779af50a7530551e879a4a6e204ee8a9e12cc
Andrew Watson <watson.arw@gmail.com>
2024-04-22 22:27:52 +1000

    Run any scripts in modules

--- setup/Ω.modules.sh
+++ setup/Ω.modules.sh
@@ -5,7 +5,7 @@
 main() {
   h1 "Setting up modules"
 
-  run_executable_files "${root_dir}/modules/**/setup.sh"
+  run_executable_files "${root_dir}/modules/**/*.sh"
 
   green_tick "Done setting up modules"
 }
