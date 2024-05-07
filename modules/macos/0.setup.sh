1a3c956ae9a845a667eddafe7f91e8cac0997a1f
Andrew Watson <watson.arw@gmail.com>
2024-05-07 13:39:35 +1000

    More MacOS keyboard settings

--- modules/macos/0.setup.sh
+++ modules/macos/0.setup.sh
@@ -66,6 +66,12 @@ macos_keyboard_setup() {
 
   echo "Disable press-and-hold for keys in favor of key repeat"
   defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+  echo "Use Function keys as Function keys"
+  defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
+
+  echo "Enable Use keyboard navigation to move focus between controls"
+  defaults write -g AppleKeyboardUIMode -int 2
 }
 
 ###############################################################################
