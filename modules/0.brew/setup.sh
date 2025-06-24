53b36147a2c0a85cae08362b891fc306f912d956
Andrew Watson <watson.arw@gmail.com>
2025-06-25 00:14:05 +1000

    Cleanup unnecessary brew commons

--- modules/0.brew/setup.sh
+++ modules/0.brew/setup.sh
@@ -2,6 +2,23 @@
 
 . "$(dirname "$0")"/../../commons.sh
 
+brew_location() {
+  # Adapted from https://github.com/ohmyzsh/ohmyzsh/blob/HEAD/plugins/brew/brew.plugin.zsh
+  local _brew_location=''
+
+  if [[ -x /opt/homebrew/bin/brew ]]; then
+    _brew_location="/opt/homebrew/bin/brew"
+  elif [[ -x /usr/local/bin/brew ]]; then
+    _brew_location="/usr/local/bin/brew"
+  elif [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+    _brew_location="/home/linuxbrew/.linuxbrew/bin/brew"
+  elif [[ -x "$HOME/.linuxbrew/bin/brew" ]]; then
+    _brew_location="$HOME/.linuxbrew/bin/brew"
+  fi
+
+  echo $_brew_location
+}
+
 install_homebrew() {
   bold "Checking for active brew installation"
   if ! command_exists brew; then
