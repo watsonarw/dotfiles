80c6db16a69994d822a0d554f2b3647b9f5eb1b0
Andrew Watson <watson.arw@gmail.com>
2026-05-21 10:02:11 +1000

    consolidate mise setup and activation scripts

--- modules/0.mise/1.setup-mise-and-tools.sh
+++ modules/0.mise/1.setup-mise-and-tools.sh
@@ -6,7 +6,36 @@ lib_dir="$script_dir/../../lib"
 . "$lib_dir/lib-loader.sh"
 load_libs "$lib_dir"
 
+readonly mise_install_path="${HOME}/.local/bin/mise"
 readonly mise_conf_dir="${XDG_CONFIG_HOME:-$HOME/.config}/mise/conf.d"
+
+install_and_activate_mise() {
+  if ! command_exists mise; then
+    echo "Mise is not active"
+
+    bold "Checking for existing mise installation"
+
+    if [ ! -e "${mise_install_path}" ]; then
+      bold "Installing mise"
+      curl https://mise.run | sh
+    else
+      echo "Mise is installed at ${mise_install_path}, skipping install..."
+    fi
+
+    bold "Activating mise"
+    activate_mise
+  fi
+
+  mise self-update -y
+
+  green_tick "Mise is installed and activated"
+}
+
+activate_mise() {
+  # Activate for bash instead of zsh, only for the script
+  eval "$(${mise_install_path} activate bash)"
+}
+
 
 link_mise_config_with_module_name() {
   local source_path=$1
@@ -44,8 +73,12 @@ cleanup_old_versions() {
 }
 
 main() {
-  h1 "Setting up mise tools"
 
+  h1 "Setting up ${script_dir}"
+  h2 "Installing and activating mise"
+  install_and_activate_mise
+
+  h2 "Setting up tools"
   setup_modular_mise_config
   install_tool_versions
   cleanup_old_versions
